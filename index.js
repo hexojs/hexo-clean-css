0f16ace31a49b8b20eff660a3fca93801a280c17
Victor Felder <victorfelder@gmail.com>
2016-01-17 11:31:58 +0100

    Add plugin priority

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var assign = require('object-assign');
 
 hexo.config.clean_css = assign({
-  exclude: ['*.min.css']
+  exclude: ['*.min.css'],
+  priority : 10
 }, hexo.config.clean_css);
 
-hexo.extend.filter.register('after_render:css', require('./lib/filter'));
+hexo.extend.filter.register('after_render:css', require('./lib/filter'), hexo.config.clean_css.priority);
