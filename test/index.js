3afdfdb1552f0ac71b74480cecd5a62e05f9c811
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-01 11:38:31 +0930

    test: drop gulp

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const should = require('chai').should(); // eslint-disable-line
+
+describe('hexo-clean-css', () => {
+  // Tests.
+  it('should minify css.', () => {
+    return true;
+  });
+});
